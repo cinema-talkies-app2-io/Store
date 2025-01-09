74c6297abd4d374bbecf2739d68125ab8d252bd8
cinema-talkies-app2-io <cinema.talkies.app2.io@gmail.com>
2025-01-09 18:45:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://passive-dorisa-cinematalkies-33564b40.koyeb.app/")
+URL = environ.get("URL", "https://empirical-tortoise-cinematalkies-adf94198.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
